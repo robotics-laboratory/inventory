d4ce85094043ba194f4725a958c6f2d0a8f481c7
AndBondStyle <andbondstyle@gmail.com>
2023-04-03 22:33:07 +0300

    Create item mvp

--- inventory/bot.py
+++ inventory/bot.py
@@ -1,3 +1,5 @@
+import asyncio
+from pathlib import Path
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -10,7 +12,17 @@ from telegram.ext import (
 )
 from inventory import orm
 from loguru import logger
-from inventory.container import Container, Provide, inject
+from inventory.container import (
+    Container,
+    Provide,
+    inject,
+    PostgresqlDatabase,
+    NotionClient,
+    S3FileSystem,
+)
+from uuid import uuid4
+
+from inventory.enums import ItemType
 
 
 class WhitelistHandler(BaseHandler):
@@ -39,6 +51,81 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"Hello, {user.name}")
 
 
+async def upload_image(
+    s3_image_path: Path,
+    tg_file: ...,
+    s3fs: S3FileSystem = Provide[Container.s3fs],
+):
+    try:
+        with s3fs.open(s3_image_path, "wb") as s3_file:
+            await tg_file.download_to_memory(s3_file)
+    except:
+        logger.exception("Image upload failed")
+
+
+@inject
+async def add_item(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    s3_public_url: str = Provide[Container.settings.s3_public_url],
+    database: PostgresqlDatabase = Provide[Container.database],
+):
+    # Спарсить текст и картинку (* не разобрали случай без картинки)
+    # Залить картинку
+    # Сгенерировать ID из БД
+    # Создать страничку
+    # Коммитнуть в БД
+    # Отправить сообщение с ID и ссылкой на страничку
+
+    if not update.message.photo:
+        await update.message.reply_text("Photo required")
+        return
+
+    with database.transaction():
+        item_name = update.message.caption[:200]  # TODO: Check length
+        item = orm.InventoryItem(name=item_name, type=ItemType.OBJECT)
+        item.save()
+        msg = await update.message.reply_text(f"⌛ Item ID: {item.id}: uploading...")
+
+        image = update.message.photo[-1]
+        tg_file = await context.bot.get_file(image.file_id)
+        ext = tg_file.file_path.split(".")[-1]
+        s3_image_path = Path(f"<bucket>/images/{uuid4()}.{ext}")
+        s3_image_url = s3_public_url + f"images/{s3_image_path.name}"
+        await upload_image(s3_image_path, tg_file)
+
+        notion_url = await create_notion_page(f"{item.id:05d}", item_name, s3_image_url)
+        await msg.edit_text(f"✅ Item ID: {item.id}: {notion_url}")
+
+
+
+@inject
+async def create_notion_page(
+    item_id: str,
+    item_name: str,
+    image_url: str,
+    notion: NotionClient = Provide[Container.notion],
+    notion_database_id: str = Provide[Container.settings.notion_database_id],
+):
+    response = await notion.pages.create(
+        parent={"database_id": notion_database_id},
+        properties={
+            "ID": {"rich_text": [{"text": {"content": item_id}}]},
+            "Name": {"title": [{"text": {"content": item_name}}]},
+        },
+        children=[
+            {
+                "type": "image",
+                "image": {
+                    "type": "external",
+                    "external": {"url": image_url},
+                },
+            }
+        ],
+    )
+    return response.get("url")
+
+
 @inject
 def init_bot(
     telegram_token: str = Provide[Container.settings.telegram_token],
@@ -48,4 +135,5 @@ def init_bot(
     app = ApplicationBuilder().token(telegram_token).build()
     app.add_handler(WhitelistHandler())
     app.add_handler(CommandHandler(["start", "help"], hello))
+    app.add_handler(MessageHandler(None, add_item))
     return app
--- inventory/container.py
+++ inventory/container.py
@@ -4,15 +4,23 @@ from dependency_injector import providers
 from dependency_injector.wiring import Provide, inject  # noqa
 from pydantic import BaseSettings
 from inventory.orm import database
+from peewee import PostgresqlDatabase  # noqa
 from playhouse import db_url
 from loguru import logger
+from notion_client import AsyncClient as NotionClient
+from s3_parse_url import parse_s3_dsn
+from s3_parse_url.ext.clients import get_boto_client_kwargs
+from s3fs import S3FileSystem
 import sys
 
 
 class Settings(BaseSettings):
-    postgres_dsn: str = None
-    notion_dsn: str = None
-    telegram_token: str = None
+    postgres_dsn: str
+    notion_token: str
+    notion_database_id: str
+    s3_dsn: str
+    s3_public_url: str
+    telegram_token: str
     logging_level: str = "info"
 
 
@@ -29,9 +37,23 @@ def init_database(dsn: str):
     database.init(**connect_kwargs)
     database.connect()
     logger.info(f"Database ready")
+    return database
+
+
+def init_s3fs(dsn: str):
+    s3_dsn = parse_s3_dsn(dsn)
+    s3fs = S3FileSystem(
+        key=s3_dsn.access_key_id,
+        secret=s3_dsn.secret_access_key,
+        client_kwargs=get_boto_client_kwargs(s3_dsn),
+    )
+    s3fs.mkdir("<bucket>/images")
+    return s3fs
 
 
 class Container(DeclarativeContainer):
     settings: Settings = providers.Configuration()
     logging = providers.Resource(init_logging, level=settings.logging_level)
     database = providers.Resource(init_database, dsn=settings.postgres_dsn)
+    notion = providers.Resource(NotionClient, auth=settings.notion_token)
+    s3fs = providers.Resource(init_s3fs, dsn=settings.s3_dsn)
